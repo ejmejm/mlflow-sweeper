48e7b1a214b46fca3b23506f25fb98c4fc603fd4
ejmejm <ninjaedan@aol.com>
2026-06-20 15:43:14 -0600

    Expose delete sweep

--- src/mlflow_sweeper/__init__.py
+++ src/mlflow_sweeper/__init__.py
@@ -1,8 +1,8 @@
 """mlflow-sweeper package."""
 
 from mlflow_sweeper.config import SweepConfig
-from mlflow_sweeper.runner import run_sweep
+from mlflow_sweeper.runner import delete_sweep, run_sweep
 from mlflow_sweeper.sweep import main
 
-__all__ = ["SweepConfig", "main", "run_sweep"]
+__all__ = ["SweepConfig", "main", "run_sweep", "delete_sweep"]
 
